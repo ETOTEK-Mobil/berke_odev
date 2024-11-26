51e4d3251b9a525ee01b545221df50719dfadeb9
berkeenesakt <github-action@actions-template-sync.noreply.github.com>
2024-11-26 11:29:57 +0000

    chore(template): merge template changes :up:
    Signed-off-by: berkeenesakt <github-action@actions-template-sync.noreply.github.com>

--- lib/ui/view/hafta_1/hafta_1_view.dart
+++ lib/ui/view/hafta_1/hafta_1_view.dart
@@ -1,34 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:odev/core/constants/app_sizes.dart';
-import 'package:odev/ui/view/hafta_1/pages/row_column_stack_page.dart';
-import 'package:odev/ui/view/hafta_1/pages/state_management/state_management_page.dart';
 
 class Hafta1View extends StatelessWidget {
   const Hafta1View({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const RowColumnStackPage()));
-              },
-              child: const Text('Row - Column - Stack Kullanimi'),
-            ),
-            const SizedBox(height: AppSize.md),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const StateManagementPage()));
-              },
-              child: const Text('State Management'),
-            ),
-          ],
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
